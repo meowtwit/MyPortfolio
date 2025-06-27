bda597ffea0fd7e24e728e330366ffb7927ac58e
meowtwit <meowtwit222@gmail.com>
2025-06-27 14:21:29 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    // 拡張するファイルが増えたらここに追加
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
